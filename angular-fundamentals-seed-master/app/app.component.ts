3903f50647363dc70f21cda8c74abac3b69d2717
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-04-26 14:46:45 +0100

    Angular fundamentals - one way data binding

--- angular-fundamentals-seed-master/app/app.component.ts
+++ angular-fundamentals-seed-master/app/app.component.ts
@@ -2,13 +2,43 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      <h1>Sup {{title + '!'}}</h1>
+
+      <button (click)="handleClickEvent()">
+        <span>Change name</span>
+      </button>
+
+      <input type="text"
+             [value]="name"
+             (input)="handleInput($event)"
+             (blur)="handleBlur($event)"
+      >
+      <div>{{name}}</div>
     </div>
-  `
+  `,
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  title: string;
+  public name: string = 'Name';
 
+  constructor() {
+    this.title = 'Title';
+  }
+
+  public handleBlur($event: FocusEvent) {
+    this.name = event.target.value;
+    console.log(event);
+  }
+
+  public handleInput($event: Event) {
+    this.name = event.target.value;
+    console.log(event);
+  }
+
+  public handleClickEvent() {
+    this.name = 'Updated Name';
+  }
 }
--- angular-fundamentals-seed-master/app/app.module.ts
+++ angular-fundamentals-seed-master/app/app.module.ts
@@ -1,17 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
   declarations: [
     AppComponent
-  ]
+  ],
+  imports:[
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
