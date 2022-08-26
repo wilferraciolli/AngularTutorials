6f46808c56ea6ccbc6c450e15ac03966f069305c
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-08-26 15:31:07 +0100

    Angular tutorials - angular fundamentals - bootstrap the application

--- Ultimate Courses/angular-fundamentals/app/app.module.ts
+++ Ultimate Courses/angular-fundamentals/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   bootstrap: [
     AppComponent
