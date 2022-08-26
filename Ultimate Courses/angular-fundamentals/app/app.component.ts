87f2773c6c6f0ef2dff405f5ff8b837d8bd094d2
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-08-26 15:52:38 +0100

    Angular tutorials - angular fundamentals - One way data binding

--- Ultimate Courses/angular-fundamentals/app/app.component.ts
+++ Ultimate Courses/angular-fundamentals/app/app.component.ts
@@ -5,14 +5,38 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-       {{title}}
+      <button (click)="handleClick($event)">
+        Change Name
+      </button>
+
+      <input
+        type="text"
+        [value]="name"
+        (input)="handleInput($event)"
+        (blur)="handleBlur($event)"
+      >
+
+      <div>{{name}}</div>
     </div>
   `
 })
 export class AppComponent {
-  title: string;
+  name: string = 'Test';
 
-  constructor() {
-    this.title= 'Ultimate Angular';
+  // this will be handled each time a user leaves the inmput box
+  public handleBlur(event: any): void {
+    this.name = event.target.value;
+    console.log(event);
+  }
+
+  // this will be triggered every input of the keyboar
+  public handleInput(event: any) {
+    this.name = event.target.value;
+  }
+
+  public handleClick(event: MouseEvent) {
+    this.name = 'button clicked';
+
+    console.log(event);
   }
 }
