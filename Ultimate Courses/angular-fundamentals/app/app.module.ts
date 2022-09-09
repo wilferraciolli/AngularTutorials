219993763f26e08046c202739c416fd9aa34ac21
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-09-09 15:06:28 +0100

    Angular tutorials - create custom modules

--- Ultimate Courses/angular-fundamentals/app/app.module.ts
+++ Ultimate Courses/angular-fundamentals/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 @NgModule({
   imports: [
+    // angular modules
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    // custom modules
+    PassengerDashboardModule
   ],
   bootstrap: [
     AppComponent
