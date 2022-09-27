dd646671c1cada3b8c1fad7084541e0537930529
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-09-27 11:24:55 +0100

    Angular tutorials - Forms part 4 Select element with values and seleced options

--- Ultimate Courses/angular-fundamentals/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ Ultimate Courses/angular-fundamentals/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Baggage } from '../../models/baggage.interface';
 import { Passenger } from '../../models/passenger.interface';
 import { PassengerDashboardService } from '../../passenger-dashboar.service';
 
@@ -48,7 +49,30 @@ import { PassengerDashboardService } from '../../passenger-dashboar.service';
                  name="checkInDate"
                  [ngModel]="detail?.checkInDate"
           >
+        </div>
 
+        <!-- luggage, used twice to show the power of ngValue -->
+        <div>
+          Luggage:
+          <select name="baggage"
+                  [ngModel]="detail?.baggage">
+            <option
+              *ngFor="let item of baggage"
+              [value]="item.key"
+              [selected]="item.key === detail?.baggage">
+              {{ item.value }}
+            </option>
+          </select>
+
+          <!-- angular specific way to bind the selected and value using ngValue -->
+          <select name="baggage"
+                  [ngModel]="detail?.baggage">
+            <option
+              *ngFor="let item of baggage"
+              [ngValue]="item.key">
+              {{ item.value }}
+            </option>
+          </select>
         </div>
 
         {{ form.value | json }}
@@ -59,6 +83,26 @@ import { PassengerDashboardService } from '../../passenger-dashboar.service';
 export class PassengerFormComponent implements OnInit {
   @Input()
   detail: Passenger;
+
+  baggage: Baggage[] = [
+    {
+      key: 'none',
+      value: 'No baggage'
+    },
+    {
+      key: 'hand-only',
+      value: 'Hand baggage'
+    },
+    {
+      key: 'hold-only',
+      value: 'Hold baggage'
+    },
+    {
+      key: 'hand-hold',
+      value: 'Hand and hold baggage'
+    }
+
+  ];
 
   constructor(private passengerService: PassengerDashboardService) {
   }
