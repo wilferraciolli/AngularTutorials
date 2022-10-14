fcda56641349040cf6345cac3d0ca0b4d535a2c5
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-10-14 14:57:47 +0100

    Angular pro - Dynamicaly create Components - using ngTemplate to pass a component to ngOutlet on a ngTemplate

--- Ultimate Courses/angular-pro/app/app.component.ts
+++ Ultimate Courses/angular-pro/app/app.component.ts
@@ -1,44 +1,23 @@
-import {
-  AfterContentInit,
-  Component,
-  ComponentFactoryResolver,
-  ComponentRef, TemplateRef,
-  ViewChild,
-  ViewContainerRef
-} from '@angular/core';
-import { read } from 'fs';
-import { AuthFormComponent } from './auth-form/auth-form.component';
-import { User } from './auth-form/auth-form.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      <div #entry></div>
+      <!-- manually pass a component to the ngcontainer -->
+      <ng-container [ngTemplateOutlet]="tmpl">
+      </ng-container>
 
       <template #tmpl>
-        Wiliam Ferraciolli: Cy
+        Wiliam Ferraciolli : CY
       </template>
 
     </div>
   `
 })
-export class AppComponent implements AfterContentInit {
-  @ViewChild('entry', { read: ViewContainerRef })
-  entry: ViewContainerRef;
+export class AppComponent {
 
-  @ViewChild('tmpl')
-  tmpl: TemplateRef<any>;
-
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+  constructor() {
   }
-
-
-  public ngAfterContentInit(): void {
-    // create a dynamic component
-    this.entry.createEmbeddedView(this.tmpl);
-  }
-
-
 }
