961654cdb6b9ca6340217d2772ca62a393469f81
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-11-10 08:48:47 +0000

    Angular pro - Testing set up

--- Ultimate Courses/angular-pro/app/stock-inventory/components/stock-counter/stock-counter.component.spec.ts
+++ Ultimate Courses/angular-pro/app/stock-inventory/components/stock-counter/stock-counter.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
+import { DebugElement } from '@angular/core';
 import { StockCounterComponent } from './stock-counter.component';
+import { By } from '@angular/platform-browser';
 
 TestBed.initTestEnvironment(
   BrowserDynamicTestingModule,
@@ -12,6 +13,7 @@ describe('StockCounterComponent', () => {
 
   let component: StockCounterComponent;
   let fixture: ComponentFixture<StockCounterComponent>;
+  let el: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,51 +24,24 @@ describe('StockCounterComponent', () => {
 
     fixture = TestBed.createComponent(StockCounterComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
 
     component.value = 0;
   });
 
-  it('should increment correctly', () => {
-    component.increment()
+  it('should increment when the + button is clicked', () => {
+    el.query(By.css('button:first-child')).triggerEventHandler('click', null);
+    fixture.detectChanges();
     expect(component.value).toBe(1);
+    expect(el.query(By.css('p')).nativeElement.textContent).toBe('1');
   });
 
-  it('should decrement correctly', () => {
-    component.increment()
+  it('should increment the value when the up arrow is pressed', () => {
+    const event = new Event('KeyboardEvent') as any;
+    event.code = 'ArrowUp';
+    el.query(By.css('.stock-counter > div > div')).triggerEventHandler('keydown', event);
+    fixture.detectChanges();
     expect(component.value).toBe(1);
-    component.decrement()
-    expect(component.value).toBe(0);
-  });
-
-  it('should not decrement below the minimum value', () => {
-    component.increment()
-    expect(component.value).toBe(1);
-    component.decrement()
-    expect(component.value).toBe(0);
-    component.decrement()
-    expect(component.value).toBe(0);
-  });
-
-  it('should not increment below the maximum value', () => {
-    for (let i = 0; i < 200; i++) {
-      component.increment();
-    }
-    expect(component.value).toBe(100);
-  });
-
-  it('should not increment over the maximum value', () => {
-    component.step = 20;
-    component.max = 20;
-    component.increment();
-    component.increment();
-    expect(component.value).toBe(20);
-  });
-
-  it('should call the output on a value change', () => {
-    spyOn(component.changed, 'emit').and.callThrough();
-    component.step = 100;
-    component.increment();
-    expect(component.changed.emit).toHaveBeenCalledWith(100);
   });
 
 });
