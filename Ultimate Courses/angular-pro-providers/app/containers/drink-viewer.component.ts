63f42857a9cbc8eb1214631ebf4d88a321bca16d
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-11-11 08:01:33 +0000

    Angular pro providers - Injection using the provider and useExisting

--- Ultimate Courses/angular-pro-providers/app/containers/drink-viewer.component.ts
+++ Ultimate Courses/angular-pro-providers/app/containers/drink-viewer.component.ts
@@ -14,17 +14,14 @@ export function DrinkFactory(http) {
   return new FoodService(http, '/api/drinks');
 }
 
+export abstract class DrinkService {
+  getDrinks: () => Observable<Drink[]>
+}
+
 @Component({
   selector: 'drink-viewer',
   providers: [
-    // a specific food provider for this instance
-    // FoodService
-    // { provide: FoodService, useClass: FoodService }
-    {
-      provide: FoodService,
-      useFactory: DrinkFactory,
-      deps: [Http]
-    }
+    { provide: FoodService, useExisting: FoodService }
   ],
   template: `
     <div>
@@ -38,10 +35,10 @@ export function DrinkFactory(http) {
 export class DrinkViewerComponent implements OnInit {
   items$: Observable<Drink[]>;
 
-  constructor(private foodService: FoodService) {
+  constructor(private foodService: DrinkService) {
   }
 
   ngOnInit() {
-    this.items$ = this.foodService.getFood();
+    this.items$ = this.foodService.getDrinks();
   }
 }
--- Ultimate Courses/angular-pro-providers/app/food.service.ts
+++ Ultimate Courses/angular-pro-providers/app/food.service.ts
@@ -17,4 +17,17 @@ export class FoodService {
     return this.http.get(this.api)
                .map(response => response.json());
   }
+
+  getSides(): Observable<any[]> {
+    return this.http.get('/api/sides')
+               .map(response => response.json());
+  }
+  getPizzas(): Observable<any[]> {
+    return this.http.get('/api/pizzas')
+               .map(response => response.json());
+  }
+  getDrinks(): Observable<any[]> {
+    return this.http.get('/api/drinks')
+               .map(response => response.json());
+  }
 }
