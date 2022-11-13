2fc68971387b89e2d942e2706b8d0b172352b329
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-11-13 14:21:57 +0000

    Calendar Example - Added components for the calendar

--- angular-calendar-exmaple/src/app/app-routing.module.ts
+++ angular-calendar-exmaple/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DayViewComponent } from './day-view/day-view.component';
+import { MonthlyViewComponent } from './montly-view/monthly-view.component';
+import { WeeklyViewComponent } from './weekly-view/weekly-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'monthly', component: MonthlyViewComponent },
+  { path: 'weekly', component: WeeklyViewComponent },
+  { path: '', redirectTo: '/monthly', pathMatch: 'full' },
+  { path: 'day', component: DayViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- angular-calendar-exmaple/src/app/app.module.ts
+++ angular-calendar-exmaple/src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { MonthlyViewComponent } from './montly-view/monthly-view.component';
+import { WeeklyViewComponent } from './weekly-view/weekly-view.component';
+import { DayViewComponent } from './day-view/day-view.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonthlyViewComponent,
+    WeeklyViewComponent,
+    DayViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
