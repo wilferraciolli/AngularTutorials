c2c6b75cbc6cc5805101c09cc2482e8e881ebd3d
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-12-02 14:15:01 +0000

    ngXAngularTutorial - Added tests for the service get http

--- ngxSotreTutorial/src/app/event/services/event.service.spec.ts
+++ ngxSotreTutorial/src/app/event/services/event.service.spec.ts
@@ -1,20 +1,47 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Attendee } from '../../models';
 
 import { EventService } from './event.service';
 
 describe('EventService', () => {
-  let service: EventService;
+  let httpTestingController: HttpTestingController;
+  let eventService: EventService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [EventService]
     });
-    service = TestBed.inject(EventService);
+    eventService = TestBed.inject(EventService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(eventService).toBeTruthy();
+  });
+
+  it('should get attendees', () => {
+    const testAttendees: Attendee[] = [
+      {
+        name: 'Test Data',
+        attending: true,
+        guests: 1
+      }
+    ];
+
+    // subscribe to the observable
+    eventService.getAttendees().subscribe();
+
+    const req = httpTestingController.expectOne('api/attendees');
+
+    // make sure that the request was the right path and method
+    expect(req.request.method).toEqual('GET');
+
+    // clean up
+    req.flush(testAttendees);
+
+    // check that there were no more pending requests
+    httpTestingController.verify();
   });
 });
