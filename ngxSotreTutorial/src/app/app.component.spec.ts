e55288ca5bd60e08efbf0d78d796b0a4485430dc
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2022-12-02 10:47:39 +0000

    ngXAngularTutorial - initial commit

--- ngxSotreTutorial/src/app/app.component.spec.ts
+++ ngxSotreTutorial/src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngxSotreTutorial'`, () => {
+  it(`should have as title 'title'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ngxSotreTutorial');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ngxSotreTutorial app is running!');
   });
 });
--- ngxSotreTutorial/src/app/app.module.ts
+++ ngxSotreTutorial/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,11 @@ import { HomeComponent } from './home/containers/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
+      { path: 'home', component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
