8ef498190d4b921b0badaa8a4f147df6f7b2f988
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-01-20 15:44:12 +0000

    Ultimate courses - Array functions

--- Ultimate Courses/typescript-basics/src/app.ts
+++ Ultimate Courses/typescript-basics/src/app.ts
@@ -1,1 +1,21 @@
-console.log('Hi yo');
+const pizzas = [
+  { name: 'Pepperoni', toppings: ['pepperoni'] }
+];
+
+// const mappedPizzas = pizzas.map((pizza) => {
+//   return pizza.name.toUpperCase();
+// });
+// the above was converted to this
+const mappedPizzas = pizzas.map(pizza =>  pizza.name.toUpperCase());
+// console.log(mappedPizzas);
+
+const pizza = {
+  name: 'Blazing inferno',
+  getName: () => pizza.name
+};
+
+console.log(pizza.getName());
+
+
+
+
