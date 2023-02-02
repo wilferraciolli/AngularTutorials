21f4bb0865e0297da5699dbc2218e7177fc334ef
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-02-02 12:11:19 +0000

    Ultimate courses - typescript master - Pick classes

--- Ultimate Courses/typescript-masterclass/src/app.ts
+++ Ultimate Courses/typescript-masterclass/src/app.ts
@@ -1,24 +1,21 @@
 interface Person {
   name: string;
   age?: number;
+  address: {};
 }
 
-// this is not needed as typescript can do it
-type MyRequired<T> = {
-  [P in keyof T]-?: T[P];
+// this is not needed as typescript can do it - this is jsut a function to pass an object, then says that the second argument must be a key of the object
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
 };
 
-// method to print a property that is optional ** but we wanted to make it required
-function printAge(person: MyRequired<Person>) {
-  return `${ person.name } is ${ person.age }`;
-}
 
-const person:  MyRequired<Person> = {
+// Allows to pick which properties we want to pick from an object
+const person: MyPick<Person, 'name' | 'age'> = {
   name: 'wil',
   age: 37
 };
 
-const ager = printAge(person);
 
 
 
