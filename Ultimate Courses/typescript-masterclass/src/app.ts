8e172fda041dea33c5c17e8e3719a11963a8ff31
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-02-02 11:31:27 +0000

    Ultimate courses - typescript master - mapped partial, showing how the Partial class works

--- Ultimate Courses/typescript-masterclass/src/app.ts
+++ Ultimate Courses/typescript-masterclass/src/app.ts
@@ -3,30 +3,26 @@ interface Person {
   age: number;
 }
 
-// this is not needed as typescript can create an object Readonly<Person>
-interface ReadOnlyPerson {
-  readonly name: string;
-  readonly age: number;
+// this is not needed as typescript can create an object Partial<Person>
+interface PartialPerson {
+  name?: string;
+  age?: number;
 }
 
-// mutable person
 const person: Person = {
   name: 'Wil',
   age: 27
 };
 
-type MyReadOnly<T> = {
-  // manual way of looping through properties and mark them as read only
-  readonly [P in keyof T]: T[P];
+type MyPartial<T> = {
+  // manual way of looping through properties and mark them as optional
+  readonly [P in keyof T]?: T[P];
 };
 
-
-// function to get a mutable object, then create a new object of same type and properties but with ReadOnly properties
-function freezeObject<T>(objectToMakeReadOnly: T): MyReadOnly<T> {
-  //  this method will take an object, then loop through all its propertyu and create a new object with Read only properties
-  return Object.freeze(objectToMakeReadOnly);
+function updatePerson(person: Person, prop: MyPartial<Person>) {
+  return { ...person, ...prop };
 }
 
-const immurablePerson = freezeObject(person);
+updatePerson(person, { name: 'NewName' });
 
 
