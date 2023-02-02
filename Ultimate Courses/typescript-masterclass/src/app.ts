34d62713d9bb365de8cafb05271ff1851f5f8ed3
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-02-02 10:44:06 +0000

    Ultimate courses - typescript master - keyof, typeof and type queries

--- Ultimate Courses/typescript-masterclass/src/app.ts
+++ Ultimate Courses/typescript-masterclass/src/app.ts
@@ -1,11 +1,28 @@
-const elem = document.querySelector('.click');
+// create an anonymus type
+const person = {
+  name: 'Wil',
+  age: 37
+};
 
-// syntax for typescript to allow the 'this' keyword to be infered, the first argument of the function is still the event: Event
-function handleClick(this: HTMLAnchorElement, event: Event) {
-  event.preventDefault();
+// type query
+type Person = typeof person; // type of, will give an interface of an object
 
-  // need to specify the argument type to resolve this
-  console.log(this.href);
+// The below is getting the keys of a property, then the below is getting the type of each key, this can be used to look up objects
+type PersonKeys = keyof Person; // union type [ name | age ], this will give the key names of the properties
+type PersonTypes = Person[PersonKeys]; // union type [ string | number ], this will give the type of each key
+
+// using generics to work out types on the fly, this will use keyof to work out the properties name durinf compile,
+// so typescript can know the value of each key before compiling, enforcing type safe
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
 }
 
-elem.addEventListener('click', handleClick, false);
+// this method will work as this is a type sage look up type since we are declatring the key of
+const personName = getProperty(person, 'name');
+const personAge = getProperty(person, 'age');
+// const personAge = getProperty(person, 'unknownProperty'); // this wont compile because 'unknownProperty is not typeof person'
+
+
+
+
+
