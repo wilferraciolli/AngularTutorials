4484a48134ef34391db94984d62612de7292a974
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-02-17 16:37:32 +0000

    Ultimate courses - typescript master - Enums using strings

--- Ultimate Courses/typescript-masterclass/src/app.ts
+++ Ultimate Courses/typescript-masterclass/src/app.ts
@@ -1,23 +1,15 @@
-
-// function overload *******************************
-
-
-// just declare function to allow JS compiler to know what the parameters the function will take
-function reverse(str: string): string;
-// just declare function
-function reverse<T>(arr: T[]): T[];
-// implementation of the function based on the declarations above
-function reverse<T>(stringOrArray: string | T[]): string | T[] {
-  if (typeof stringOrArray === 'string') {
-    return stringOrArray
-    .split('')
-    .reverse()
-    .join('');
-  }
-
-  // call the reverse just to be able to return a new array
-  return stringOrArray.slice().reverse();
+const enum Sizes {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
 }
 
-reverse('Pepperoni');
-reverse(['bacon', 'pepperoni', 'chili', 'mushrooms']);
+let selected: Sizes = Sizes.Small;
+
+function updateSize(size: Sizes): void {
+  selected = size;
+}
+
+updateSize(Sizes.Large);
+
+console.log(selected);
