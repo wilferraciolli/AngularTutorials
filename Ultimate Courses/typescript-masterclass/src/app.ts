4e1365a48d2aea4776baf284d299d9d2e00a7612
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-02-17 16:02:22 +0000

    Ultimate courses - typescript master - generics

--- Ultimate Courses/typescript-masterclass/src/app.ts
+++ Ultimate Courses/typescript-masterclass/src/app.ts
@@ -1,24 +1,45 @@
-interface Item {
-  name: string;
-}
-
-// only interfaces can extend
-interface Artist extends Item {
-  songs: number;
-}
-interface Artist { // interface Artis can be declared twice
-getSongs(): number;
-}
-
-// for types, intersection needs to be used when want to extend
-type Artist2 = { name: string } & Item;
-
-
-// when using interfdces that are duplicated, then all of properties are merged together
-const newArtist: Artist = {
-  name: 'ABC',
-  songs: 5,
-  getSongs() {
-    return this.songs;
+class Pizza {
+  constructor(private name: string, private price: number) {
   }
-};
+}
+
+// // a list that will take anything, however generics can specify a type when instantiated
+// class List {
+//   list: any[];
+//
+//   addItem(item: any): void {
+//     this.list.push(item);
+//   }
+//
+//   getList(): any[] {
+//     return this.list;
+//   }
+// }
+
+//generics can specify a type when instantiated
+class List<T> {
+  private list: T[];
+
+  addItem(item: any): void {
+    this.list.push(item);
+  }
+
+  getList(): T[] {
+    return this.list;
+  }
+}
+
+
+const list = new List<Pizza>();
+list.addItem(new Pizza('Pepperoni', 15));
+list.addItem(new Pizza('Pepperoni', 15));
+
+const pizzas = list.getList();
+
+
+class Coupon {
+  constructor(private name: string) {
+  }
+}
+const anotherList = new List<Coupon>();
+anotherList.addItem(new Coupon('Coupon'));
