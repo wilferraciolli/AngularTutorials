55e6948a653a8b32e3100a324b7976af31aad311
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-02-17 10:59:46 +0000

    Ultimate courses - typescript master - TypeOf when checkins type of vars in a class

--- Ultimate Courses/typescript-masterclass/src/app.ts
+++ Ultimate Courses/typescript-masterclass/src/app.ts
@@ -1,16 +1,38 @@
-// create our dictionary using record
-let dictionary: Record<string, TrackStates> = {};
+// ******************* type of and Type Guards
+function foo(bar: string | number) {
+  if (typeof bar === 'string') {
+    // handling is string
+  }
 
-interface TrackStates {
-  current: string;
-  next: string;
+  // here it is a number
 }
 
-// create an item that will take the key as the keys of an object, then is value
-const item: Record<keyof TrackStates, string> = {
-  current: 'js02js9',
-  next: '8nlksjsk'
-};
 
-// javascript index - numbers are coerced to string when adding to dictionary
-dictionary[0] = item;
+class Song {
+  constructor(public title: string, public duration: string | number) {
+
+  }
+}
+
+function getSongDuration(item: Song){
+  // check that the duration is of type string
+  if(typeof item.duration === 'string'){
+    return item.duration;
+  }
+
+  const {duration} = item;
+  const minutes = Math.floor(duration / 60000);
+  const seconds = (duration / 1000) % 60;
+
+  // parse the duration from milliseconds
+  return `${minutes}:${seconds}`;
+}
+
+// get song duration as string
+const songDurationFromString = getSongDuration(new Song('title', '05:41'));
+console.log(songDurationFromString);
+
+// get song duration as number
+const songDurationFromMs = getSongDuration(new Song('title', 33000));
+console.log(songDurationFromString);
+
