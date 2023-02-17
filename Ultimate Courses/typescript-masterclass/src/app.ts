bf3401a4c27c5bd624270f265fcc37c888be8075
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-02-17 12:42:37 +0000

    Ultimate courses - typescript master - interfaces vs class

--- Ultimate Courses/typescript-masterclass/src/app.ts
+++ Ultimate Courses/typescript-masterclass/src/app.ts
@@ -1,51 +1,24 @@
-interface Order {
-  id: string;
-  amount: number;
-  currency: string;
+interface Item {
+  name: string;
 }
 
-interface Stripe {
-  type: 'stripe'; // add common property to check types, this defines the only value this will ever accept
-  card: string;
-  cvc: string;
+// only interfaces can extend
+interface Artist extends Item {
+  songs: number;
+}
+interface Artist { // interface Artis can be declared twice
+getSongs(): number;
 }
 
-interface PayPal {
-  type: 'paypal'; // add common property to check types, this defines the only value this will ever accept
-  email: string;
-}
+// for types, intersection needs to be used when want to extend
+type Artist2 = { name: string } & Item;
 
-type CheckoutCard = Order & Stripe;
-type CheckoutPayPal = Order & PayPal;
 
-const order: Order = {
-  id: 'xj28s',
-  amount: 100,
-  currency: 'USD'
-};
-
-const orderCard: CheckoutCard = {
-  ...order,
-  type: 'stripe', // assign the only value available to this property
-  card: '1000 2000 3000 4000',
-  cvc: '123'
-};
-
-const orderPayPal: CheckoutPayPal = {
-  ...order,
-  type: 'paypal', // assign the only value available to this property
-  email: 'abc@def.com'
-};
-
-// define the available types
-type Payload = CheckoutCard | CheckoutPayPal;
-
-function checkout(payload: Payload) {
-  // factory create based on a common property
-  if (payload.type === 'stripe') {
-    console.log(payload.card, payload.cvc);
+// when using interfdces that are duplicated, then all of properties are merged together
+const newArtist: Artist = {
+  name: 'ABC',
+  songs: 5,
+  getSongs() {
+    return this.songs;
   }
-  if (payload.type === 'paypal') {
-    console.log(payload.email);
-  }
-}
+};
