c496f79a0b6a7ae18ca7e3880c507d57c508e134
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-02-28 11:16:32 +0000

    Ultimate courses - Augumented modules using type definitions

--- Ultimate Courses/typescript-masterclass/src/@types/lodash/index.d.ts
+++ Ultimate Courses/typescript-masterclass/src/@types/lodash/index.d.ts
@@ -1,4 +1,12 @@
 // this files allows to declare type definition for third party libraries
+
+import * as lodash from 'lodash';
+
 declare module 'lodash' {
-  export function chunck(collection: any, size?: number): any[][];
+  // export function chunck(collection: any, size?: number): any[][];
+
+  // merge lodash interface
+  interface LoDashStatic {
+    log(item: string): void;
+  }
 }
--- Ultimate Courses/typescript-masterclass/src/app.ts
+++ Ultimate Courses/typescript-masterclass/src/app.ts
@@ -1,4 +1,13 @@
 import * as _ from 'lodash';
 
 // this will be a know type because we have declared lodash types
-_.chunck([1,2,3,4], 2);
+_.chunk([1, 2, 3, 4], 2);
+
+// add our own method to lodash
+_.mixin({
+  log(item: string) {
+    console.log(':::', item);
+  }
+});
+
+_.log('item to log');
