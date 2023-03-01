1bea0a10bd951fc8c2f32890b931e1e98f83d514
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-03-01 09:59:41 +0000

    Ultimate courses - HTM 5 apis - initial commit

---
+++ Ultimate Courses/javascript-html5-apis/webpack.config.babel.js
@@ -0,0 +1,72 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin';
+
+export default {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(mp3|mp4|webm)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+      favicon: 'assets/img/favicon.png',
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'async',
+    }),
+  ],
+  stats: 'minimal',
+  devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: true,
+      errors: false,
+      errorDetails: false,
+      warnings: false,
+      publicPath: false,
+    },
+    open: false,
+    contentBase: './dist',
+    clientLogLevel: 'none',
+    historyApiFallback: true,
+    inline: true,
+    port: 4000,
+  },
+};
