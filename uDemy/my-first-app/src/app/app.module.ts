ca7694b92c711ec224007c4876226a3217a0f54f
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-05-12 11:38:06 +0100

    udemy - The Complete Angular Guide - my first app - Created components

--- uDemy/my-first-app/src/app/app.module.ts
+++ uDemy/my-first-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
