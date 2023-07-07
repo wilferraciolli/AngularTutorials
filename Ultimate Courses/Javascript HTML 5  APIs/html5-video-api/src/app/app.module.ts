f978f06a03d3eb57aa6aee5818f123741eae899f
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-07-07 14:49:46 +0100

    ultimate courses - Javascript html 5 apis - Audio element functionality

--- Ultimate Courses/Javascript HTML 5  APIs/html5-video-api/src/app/app.module.ts
+++ Ultimate Courses/Javascript HTML 5  APIs/html5-video-api/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { VideoComponent } from './video/video.component';
+import { AudioComponent } from './audio/audio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoComponent
+    VideoComponent,
+    AudioComponent
   ],
   imports: [
     BrowserModule
