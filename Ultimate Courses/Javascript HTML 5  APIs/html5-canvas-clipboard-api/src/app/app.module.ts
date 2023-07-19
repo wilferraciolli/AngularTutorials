7c0cf43847f6340456c4c95be21c2e8e506f85a6
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-07-19 13:14:08 +0100

    ultimate courses - Javascript html 5 apis - canvas Created e signature and calculated canvas x and y position

--- Ultimate Courses/Javascript HTML 5  APIs/html5-canvas-clipboard-api/src/app/app.module.ts
+++ Ultimate Courses/Javascript HTML 5  APIs/html5-canvas-clipboard-api/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './canvas/canvas.component';
+import { ESignatureComponent } from './e-signature/e-signature.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CanvasComponent
+    CanvasComponent,
+    ESignatureComponent
   ],
   imports: [
     BrowserModule
