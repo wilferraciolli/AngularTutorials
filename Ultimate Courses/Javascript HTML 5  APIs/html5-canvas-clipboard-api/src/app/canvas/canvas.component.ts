e008cd3a39a380c37a0967b6a521ba94e16e30c3
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-07-19 10:41:43 +0100

    ultimate courses - Javascript html 5 apis - canvas api draw square on canvas

--- Ultimate Courses/Javascript HTML 5  APIs/html5-canvas-clipboard-api/src/app/canvas/canvas.component.ts
+++ Ultimate Courses/Javascript HTML 5  APIs/html5-canvas-clipboard-api/src/app/canvas/canvas.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.scss']
 })
-export class CanvasComponent {
+export class CanvasComponent implements OnInit {
 
+  public ngOnInit(): void {
+    if (this.isCanvasSupported()) {
+      const canvas = document.querySelector('canvas');
+      if (canvas) {
+        const context = canvas.getContext('2d');
+        if (context) {
+          // add some formatting to the canvas
+          context.strokeStyle = '#222';
+          context.lineCap = 'round';
+          context.lineJoin = 'round';
+          context.lineWidth = 4;
+
+          // create a path and update it, this will allow to write to the canvas
+          context.beginPath();
+          context.moveTo(10, 10);
+          context.lineTo(20, 10);
+          context.lineTo(20, 20);
+          context.lineTo(10, 20);
+          context.lineTo(10, 10);
+          context.stroke();
+          context.closePath();
+
+          // draw to the canvas context
+          context.fillRect(50,50,100,100);
+        }
+      }
+    }
+  }
+
+  private isCanvasSupported(): boolean {
+    let htmlCanvasElement = window.HTMLCanvasElement;
+
+    return htmlCanvasElement !== null;
+  }
 }
