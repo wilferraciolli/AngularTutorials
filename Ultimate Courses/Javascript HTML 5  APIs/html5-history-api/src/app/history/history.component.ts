8490d22c3a0dbcc0811ea867c0e824b1cd7a0ba3
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-07-19 09:29:25 +0100

    ultimate courses - Javascript html 5 apis - history api added push and replace state to the history

--- Ultimate Courses/Javascript HTML 5  APIs/html5-history-api/src/app/history/history.component.ts
+++ Ultimate Courses/Javascript HTML 5  APIs/html5-history-api/src/app/history/history.component.ts
@@ -6,17 +6,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
+  public links!: NodeListOf<Element>;
+  public readonly players: Player[] = PLAYERS;
+
   public ngOnInit(): void {
     // check if on base root
     if (location.pathname === '/') {
-      // add data and append to the url
-      const player: Player = players[0];
+      // add data and append to the url using the replace state, it could also be pushState()
+      const player: Player = PLAYERS[0];
       history.replaceState(player, '', player.id);
+
+      // call the render state to browser
+      this.render(player);
     }
+
+    // this.setLinks(); this wont work till angular component is run
+  }
+
+  private render(state: Player): void {
+    const info = document.querySelector('.info');
+
+    if (info) {
+      info.innerHTML = `
+    <h3>${ state.name }</h3>
+    <p>${ state.bio }</p>
+    `;
+    }
+  }
+
+  private setLinks() {
+    const linksNode = document.querySelectorAll('.links a');
+    // @ts-ignore
+    this.links = [...linksNode];
+
+    // loop through each anchor tag and remove the default behaviour of navigating
+    this.links.forEach((link: any) => {
+      link.addEventListener('click', (e: any) => {
+        e.preventDefault();
+
+        // get the id of the href
+        const id = e.target.getAttribute('href').substring(1);
+        const chosenPlayer: Player | undefined = PLAYERS.find((player: Player) => player.id === id);
+
+        // add the push state to the history
+        if (chosenPlayer) {
+          history.pushState(chosenPlayer, '', chosenPlayer.id);
+          this.render(chosenPlayer);
+        }
+      });
+    });
+  }
+
+  // This method is used just to allow Angular to instantiate the component then we grab the values
+  // and add remove the behaviour from the nav links
+  public addLinks(): void {
+    this.setLinks();
   }
 }
 
-export const players: Player[] = [
+export const PLAYERS: Player[] = [
   {
     id: 'id1',
     name: 'Mario',
