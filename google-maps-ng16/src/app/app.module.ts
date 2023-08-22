ad78cbd21756ab84892dd696ec14a7407d6765ec
Wiliam Ferraciolli <wiliam334@hotmail.com>
2023-08-22 23:22:12 +0300

    google maps ng 16 - initial commit

--- google-maps-ng16/src/app/app.module.ts
+++ google-maps-ng16/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {GoogleMapsModule} from "@angular/google-maps";
+
 
 @NgModule({
   declarations: [
@@ -16,4 +18,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
