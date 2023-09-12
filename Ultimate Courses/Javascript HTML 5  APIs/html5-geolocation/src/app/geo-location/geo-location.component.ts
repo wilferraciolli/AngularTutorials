5c1308f26dd846262585ef59d46ee588a18643c2
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-09-12 17:52:28 +0100

    ultimate courses - Javascript html 5 apis - geolocation - added check geolocation is enabled and get Lat and Long values

--- Ultimate Courses/Javascript HTML 5  APIs/html5-geolocation/src/app/geo-location/geo-location.component.ts
+++ Ultimate Courses/Javascript HTML 5  APIs/html5-geolocation/src/app/geo-location/geo-location.component.ts
@@ -9,8 +9,31 @@ export class GeoLocationComponent implements OnInit {
 
   public ngOnInit(): void {
     if ('geolocation' in navigator) {
-      console.log('Geolocation enabled');
+
+      // define the handle success callback function
+      // @ts-ignore
+      const handleSuccess = ({ coords }) => {
+        const { latitude, longitude } = coords;
+        console.log(latitude, longitude);
+      };
+
+
+      this._getGeolocation(handleSuccess);
+
+
     }
   }
 
+  private _getGeolocation(successFn: ({ coords }: { coords: any }) => void): void {
+    // get the inner variable or coordinates and assign it to coords
+    // navigator.geolocation.getCurrentPosition(({ coords }) => {
+    //   console.log(coords);
+    //
+    //   // get the lat and long from coords
+    //   const { latitude, longitude } = coords;
+    //   console.log(latitude, longitude);
+    // });
+    //
+    navigator.geolocation.getCurrentPosition(successFn);
+  }
 }
