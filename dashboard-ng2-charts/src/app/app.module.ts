c36cd6992b50dc5011f45bed624b81bd64821c83
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-10-20 13:59:17 +0100

    Dashboard ng2 charts - Added table for display orders

--- dashboard-ng2-charts/src/app/app.module.ts
+++ dashboard-ng2-charts/src/app/app.module.ts
@@ -1,34 +1,39 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { NgChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { DashComponent } from './dash/dash.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
 import { CardComponent } from './card/card.component';
+import { AnnualSalesChartComponent } from './charts/annual-sales-chart/annual-sales-chart.component';
 import { ProductSalesChartComponent } from './charts/product-sales-chart/product-sales-chart.component';
 import { SalesTrafficChartComponent } from './charts/sales-traffic-chart/sales-traffic-chart.component';
-import { AnnualSalesChartComponent } from './charts/annual-sales-chart/annual-sales-chart.component';
 import { StoreSessionsChartComponent } from './charts/store-sessions-chart/store-sessions-chart.component';
+import { BarChartComponent } from './charts/templates/bar-chart/bar-chart.component';
+import { BubbleChartComponent } from './charts/templates/bubble-chart/bubble-chart.component';
+import { DoughnutChartComponent } from './charts/templates/doughnut-chart/doughnut-chart.component';
 import { LineChartComponent } from './charts/templates/line-chart/line-chart.component';
 import { PieChartComponent } from './charts/templates/pie-chart/pie-chart.component';
-import { BarChartComponent } from './charts/templates/bar-chart/bar-chart.component';
-import { DoughnutChartComponent } from './charts/templates/doughnut-chart/doughnut-chart.component';
-import { RadarChartComponent } from './charts/templates/radar-chart/radar-chart.component';
 import { PolarAreaChartComponent } from './charts/templates/polar-area-chart/polar-area-chart.component';
-import { BubbleChartComponent } from './charts/templates/bubble-chart/bubble-chart.component';
+import { RadarChartComponent } from './charts/templates/radar-chart/radar-chart.component';
 import { ScatterChartComponent } from './charts/templates/scatter-chart/scatter-chart.component';
+import { DashComponent } from './dash/dash.component';
+import { NavComponent } from './nav/nav.component';
+import { OrdersTableComponent } from './orders-table/orders-table.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashComponent }
@@ -51,7 +56,8 @@ const routes: Routes = [
     RadarChartComponent,
     PolarAreaChartComponent,
     BubbleChartComponent,
-    ScatterChartComponent
+    ScatterChartComponent,
+    OrdersTableComponent
   ],
   imports: [
     BrowserModule,
@@ -66,7 +72,11 @@ const routes: Routes = [
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
