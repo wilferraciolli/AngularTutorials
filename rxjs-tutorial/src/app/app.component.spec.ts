a3400728e06c03f12dd4a92c760487c8891b31e0
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2023-11-10 12:53:56 +0000

    rxJS tutorial - initial commit

--- rxjs-tutorial/src/app/app.component.spec.ts
+++ rxjs-tutorial/src/app/app.component.spec.ts
@@ -1,11 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [AppComponent]
   }));
 
   it('should create the app', () => {
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rx-js'`, () => {
+  it(`should have the 'rxjs-tutorial' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rx-js');
+    expect(app.title).toEqual('rxjs-tutorial');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rx-js app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('rxjs-tutorial app is running!');
   });
 });
--- rxjs-tutorial/src/app/app.component.ts
+++ rxjs-tutorial/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rx-js';
+  title = 'rxjs-tutorial';
 }
