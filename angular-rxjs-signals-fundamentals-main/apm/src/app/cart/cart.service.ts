3e5b082086f0481d298b41adfa0c7ab7612c25cb
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-01-16 16:51:52 +0000

    angular rxjs signals course - Replaced cart items and total with signals

--- angular-rxjs-signals-fundamentals-main/apm/src/app/cart/cart.service.ts
+++ angular-rxjs-signals-fundamentals-main/apm/src/app/cart/cart.service.ts
@@ -1,4 +1,4 @@
-import { effect, EffectRef, Injectable, signal, WritableSignal } from '@angular/core';
+import { computed, effect, EffectRef, Injectable, Signal, signal, WritableSignal } from '@angular/core';
 import { Product } from '../products/product';
 import { CartItem } from './cart';
 
@@ -6,17 +6,50 @@ import { CartItem } from './cart';
   providedIn: 'root'
 })
 export class CartService {
+  private baseTax: number = 10;
+  private baseDeliveryFee: number = 5.99;
+
   public cartItems: WritableSignal<CartItem[]> = signal<CartItem[]>([]);
+
+  // create a computed function to calculate the count once each item changes (+ or -)
+  public cartCount: Signal<number> = computed(() =>
+    this.cartItems().reduce((accQty: number, item: CartItem) =>
+      accQty + item.quantity, 0)
+  );
 
   // effect to test when an item is added to the cart
   public eLength: EffectRef = effect(() =>
     console.log('Cart array changed ', this.cartItems().length));
+
+  // calculate subtotal (quantity * price)
+  public subTotal: Signal<number> = computed(() =>
+    this.cartItems().reduce((accTotal: number, item: CartItem) =>
+      accTotal + (item.quantity * item.product.price), 0)
+  );
+
+  // calculate delivery fee (5.99 < 50 otherwise free)
+  public deliveryFee: Signal<number> = computed<number>(() =>
+    this.subTotal() < 50 ? this.baseDeliveryFee : 0
+  );
+
+  public tax: Signal<number> = computed<number>(() =>
+    Math.round((this.subTotal() + this.baseTax) / 100)
+  );
+
+  // calculate total Price (subtotal + delivery + tax)
+  public totalPrice: Signal<number> = computed(() =>
+    this.subTotal() + this.deliveryFee() + this.tax()
+  );
 
   public addToCart(productToAdd: Product): void {
     // to be able to update a signal, we have to get the current value, then add the spread operator to copy the current one and add the next one
     this.cartItems.update((items: CartItem[]) =>
       [...items, { product: productToAdd, quantity: 1 }]
     );
+  }
+
+  public calculateSubTotalForItem(item: CartItem): number {
+    return item.quantity * item.product.price;
   }
 
 }
