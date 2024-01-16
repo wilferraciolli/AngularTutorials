344ee6b8b014e13c83bbe0952d7438f8865844e7
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-01-16 16:03:03 +0000

    angular rxjs signals course - Created cart service to use signals

--- angular-rxjs-signals-fundamentals-main/apm/src/app/cart/cart.service.ts
+++ angular-rxjs-signals-fundamentals-main/apm/src/app/cart/cart.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from "@angular/core";
+import { effect, EffectRef, Injectable, signal, WritableSignal } from '@angular/core';
+import { Product } from '../products/product';
+import { CartItem } from './cart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  public cartItems: WritableSignal<CartItem[]> = signal<CartItem[]>([]);
+
+  // effect to test when an item is added to the cart
+  public eLength: EffectRef = effect(() =>
+    console.log('Cart array changed ', this.cartItems().length));
+
+  public addToCart(productToAdd: Product): void {
+    // to be able to update a signal, we have to get the current value, then add the spread operator to copy the current one and add the next one
+    this.cartItems.update((items: CartItem[]) =>
+      [...items, { product: productToAdd, quantity: 1 }]
+    );
+  }
 
 }
--- angular-rxjs-signals-fundamentals-main/apm/src/app/products/product-detail/product-detail.component.ts
+++ angular-rxjs-signals-fundamentals-main/apm/src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe, CurrencyPipe, NgFor, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { catchError, EMPTY, Observable } from 'rxjs';
+import { CartService } from '../../cart/cart.service';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 
@@ -13,6 +14,7 @@ import { ProductService } from '../product.service';
 export class ProductDetailComponent {
 
   private productService: ProductService = inject(ProductService);
+  private cartService: CartService = inject(CartService);
 
   public errorMessage: string = '';
   public readonly product$: Observable<Product> = this.productService.product$.pipe(
@@ -25,6 +27,7 @@ export class ProductDetailComponent {
   // public pageTitle: string = this.product ? `Product Detail for: ${ this.product.productName }` : 'Product Detail';
   public pageTitle: string = 'Page title to be changed later';
 
-  public addToCart(product: Product) {
+  public addToCart(product: Product): void {
+    this.cartService.addToCart(product);
   }
 }
