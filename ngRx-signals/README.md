9961d849bb5781f540aa5a54867afdf41d4aeb3c
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-02-01 15:32:09 +0000

    ngRx-store - initial commit renamed project

--- ngRx-signals/README.md
+++ ngRx-signals/README.md
@@ -4,3 +4,11 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## Tutorial can be found here
 https://www.youtube.com/watch?v=VPyra_5Ko-4
+
+# Dependencies
+
+### ngRx Signals
+`npm install @ngrx/signals`
+
+### Material
+`ng add @angular/material`
--- ngRx-signals/src/app/app.component.spec.ts
+++ ngRx-signals/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ngRx-store' title`, () => {
+  it(`should have the 'ngRx-signals' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngRx-store');
+    expect(app.title).toEqual('ngRx-signals');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ngRx-store app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ngRx-signals app is running!');
   });
 });
