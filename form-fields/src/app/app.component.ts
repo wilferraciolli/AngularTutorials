ff82136cd3eec257de52eb7860b76ac578a77838
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-07-24 16:51:58 +0100

    form-fields = Added date time service to convert timezones

--- form-fields/src/app/app.component.ts
+++ form-fields/src/app/app.component.ts
@@ -32,23 +32,60 @@ export class AppComponent {
 
   public ngOnInit(): void {
     console.log('users timezone: ', this._dateTimeService.resolveUsersTimezone());
-    console.log('users parse timezone Europe/London: ', this._dateTimeService.parseUTCDateTimeToTimezone('2024-01-01T09:00:00Z', 'Europe/London'));
-    console.log('users parse timezone Europe/London: ', this._dateTimeService.parseUTCDateTimeToTimezone('2024-09-01T09:00:00Z', 'Europe/London'));
-    console.log('users parse timezone Europe/London: ', this._dateTimeService.parseUTCDateTimeToTimezone('2024-11-01T09:00:00Z', 'Europe/London'));
+    console.log('timezone Europe/London: 9', this._dateTimeService.parseUTCDateTimeToTimezone('2024-01-01T09:00:00Z', 'Europe/London'));
+    console.log('timezone Europe/London: 10', this._dateTimeService.parseUTCDateTimeToTimezone('2024-09-01T09:00:00Z', 'Europe/London'));
+    console.log('timezone Europe/London: 9', this._dateTimeService.parseUTCDateTimeToTimezone('2024-11-01T09:00:00Z', 'Europe/London'));
 
-    console.log('users format timezone Europe/London: ', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-01-01T09:00', 'Europe/London'));
-    console.log('users format timezone Europe/London: ', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-09-01T09:00', 'Europe/London'));
-    console.log('users format timezone Europe/London: ', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-11-01T09:00', 'Europe/London'));
+    console.log('timezone Europe/London: 9', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-01-01T09:00', 'Europe/London'));
+    console.log('timezone Europe/London: 8', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-09-01T09:00', 'Europe/London'));
+    console.log('timezone Europe/London: 9', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-11-01T09:00', 'Europe/London'));
+
+    console.log('23:01: ', this._dateTimeService.resolveUsersTimezone());
+    console.log('timezone Europe/London: 23', this._dateTimeService.parseUTCDateTimeToTimezone('2024-01-01T23:01:00Z', 'Europe/London'));
+    console.log('timezone Europe/London: 00', this._dateTimeService.parseUTCDateTimeToTimezone('2024-09-01T23:01:00Z', 'Europe/London'));
+    console.log('timezone Europe/London: 23', this._dateTimeService.parseUTCDateTimeToTimezone('2024-11-01T23:01:00Z', 'Europe/London'));
+
+    console.log('timezone Europe/London: 23', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-01-01T23:01', 'Europe/London'));
+    console.log('timezone Europe/London: 23', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-09-02T00:01', 'Europe/London'));
+    console.log('timezone Europe/London: 23', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-11-01T23:01', 'Europe/London'));
 
 
     console.log('users timezone: ', this._dateTimeService.resolveUsersTimezone());
-    console.log('users parse timezone Asia/Nicosia: ', this._dateTimeService.parseUTCDateTimeToTimezone('2024-01-01T09:00:00Z', 'Asia/Nicosia'));
-    console.log('users parse timezone Asia/Nicosia: ', this._dateTimeService.parseUTCDateTimeToTimezone('2024-09-01T09:00:00Z', 'Asia/Nicosia'));
-    console.log('users parse timezone Asia/Nicosia: ', this._dateTimeService.parseUTCDateTimeToTimezone('2024-11-01T09:00:00Z', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 11', this._dateTimeService.parseUTCDateTimeToTimezone('2024-01-01T09:00:00Z', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 12', this._dateTimeService.parseUTCDateTimeToTimezone('2024-09-01T09:00:00Z', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 11', this._dateTimeService.parseUTCDateTimeToTimezone('2024-11-01T09:00:00Z', 'Asia/Nicosia'));
 
-    console.log('users format timezone Asia/Nicosia: ', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-01-01T09:00', 'Asia/Nicosia'));
-    console.log('users format timezone Asia/Nicosia: ', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-09-01T09:00', 'Asia/Nicosia'));
-    console.log('users format timezone Asia/Nicosia: ', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-11-01T09:00', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 7', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-01-01T09:00', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 6', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-09-01T09:00', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 7', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-11-01T09:00', 'Asia/Nicosia'));
+
+    console.log('21:01:Asia/Nicosia ', this._dateTimeService.resolveUsersTimezone());
+    console.log('timezone Asia/Nicosia: 23', this._dateTimeService.parseUTCDateTimeToTimezone('2024-01-01T21:01:00Z', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 00', this._dateTimeService.parseUTCDateTimeToTimezone('2024-09-01T21:01:00Z', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 23', this._dateTimeService.parseUTCDateTimeToTimezone('2024-11-01T21:01:00Z', 'Asia/Nicosia'));
+
+    console.log('timezone Asia/Nicosia: 21', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-01-01T23:01', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 21', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-09-02T00:01', 'Asia/Nicosia'));
+    console.log('timezone Asia/Nicosia: 21', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-11-01T23:01', 'Asia/Nicosia'));
+
+
+    console.log('users timezone: ', this._dateTimeService.resolveUsersTimezone());
+    console.log('timezone America/Sao_Paulo: 6', this._dateTimeService.parseUTCDateTimeToTimezone('2024-01-01T09:00:00Z', 'America/Sao_Paulo'));
+    console.log('timezone America/Sao_Paulo: 6', this._dateTimeService.parseUTCDateTimeToTimezone('2024-09-01T09:00:00Z', 'America/Sao_Paulo'));
+    console.log('timezone America/Sao_Paulo: 6', this._dateTimeService.parseUTCDateTimeToTimezone('2024-11-01T09:00:00Z', 'America/Sao_Paulo'));
+
+    console.log('timezone America/Sao_Paulo: 12', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-01-01T09:00', 'America/Sao_Paulo'));
+    console.log('timezone America/Sao_Paulo: 12', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-09-01T09:00', 'America/Sao_Paulo'));
+    console.log('timezone America/Sao_Paulo: 12', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-11-01T09:00', 'America/Sao_Paulo'));
+
+    console.log('23: Sao Paulo', this._dateTimeService.resolveUsersTimezone());
+    console.log('timezone America/Sao_Paulo: 23', this._dateTimeService.parseUTCDateTimeToTimezone('2024-01-01T02:01:00Z', 'America/Sao_Paulo'));
+    console.log('timezone America/Sao_Paulo: 23', this._dateTimeService.parseUTCDateTimeToTimezone('2024-09-01T02:01:00Z', 'America/Sao_Paulo'));
+    console.log('timezone America/Sao_Paulo: 23', this._dateTimeService.parseUTCDateTimeToTimezone('2024-11-01T02:01:00Z', 'America/Sao_Paulo'));
+
+    console.log('timezone America/Sao_Paulo: 02', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2023-12-31T23:01', 'America/Sao_Paulo'));
+    console.log('timezone America/Sao_Paulo: 02', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-08-31T23:01', 'America/Sao_Paulo'));
+    console.log('timezone America/Sao_Paulo: 02', this._dateTimeService.parseDateTimeFromTimezoneToUTC('2024-10-31T23:01', 'America/Sao_Paulo'));
   }
 
   get date(): FormControl<string | null> {
