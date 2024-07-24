81019eefeac15f467e6bcc46d7a082cc49f9639f
WilFerraciolli <wiliam334@hotmail.com>
2024-07-24 21:48:48 +0300

    date-time : Removed old date time template

--- date-time/src/app/date-time.form.ts
+++
@@ -1,8 +0,0 @@
-import { FormControl } from "@angular/forms";
-import { DateTime } from 'luxon';
-
-export interface DateTimeForm {
-  businessDate: FormControl<DateTime>;
-  businessTime: FormControl<DateTime>;
-  dateTime: FormControl<DateTime>;
-}
