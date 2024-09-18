0a43acbabf80445f06bc989b5c1b27c978e4bc19
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-09-18 09:06:31 +0100

    capacitor-angular17 - Made it esBuild for faster development

--- capacitor-angular-17/modern-app/src/environments/environment.ts
+++ capacitor-angular-17/modern-app/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'e1985a12d46ef884a6ef042b2ec7d258'
 };
 
 /*
