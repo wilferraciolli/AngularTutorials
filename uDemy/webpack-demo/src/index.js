1d7871b421a0e07dd6f946759a5cbf64ff14206c
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-09-20 16:17:03 +0100

    udemy - build tools: Added webpack image loader

--- uDemy/webpack-demo/src/index.js
+++ uDemy/webpack-demo/src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from'./icon.png';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // add icon
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.append(myIcon);
 
   return element;
 }
