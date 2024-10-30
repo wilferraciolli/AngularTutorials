692f7a91a49cd89d24bf0f919c1252a98bd8a260
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-10-30 08:26:46 +0000

    skeleton-loaders: Added templates

--- skeleton-loaders/src/app/components/card-list/card-list.component.ts
+++ skeleton-loaders/src/app/components/card-list/card-list.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from '@angular/material/card';
 
 @Component({
   selector: 'app-card-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardActions,
+    MatButton,
+    MatCardContent
+  ],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.scss'
 })
