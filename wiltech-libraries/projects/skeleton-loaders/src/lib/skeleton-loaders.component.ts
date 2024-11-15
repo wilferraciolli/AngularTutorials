3915c10a1789e9d2d77bffbce45cfd033dae7b8f
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-11-15 17:29:37 +0000

    wiltech-libraries-user-store: added skeleton loaders spinner

--- wiltech-libraries/projects/skeleton-loaders/src/lib/skeleton-loaders.component.ts
+++ wiltech-libraries/projects/skeleton-loaders/src/lib/skeleton-loaders.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'lib-skeleton-loaders',
   standalone: true,
-  imports: [],
+  imports: [
+    NgxSkeletonLoaderModule
+  ],
   template: `
     <p>
       skeleton-loaders works!
+      <ngx-skeleton-loader count="5" appearance="circle" />
     </p>
   `,
   styles: ``
