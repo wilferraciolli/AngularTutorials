fbc22808f5e47a9389cb86998fd29a3bddc644ba
Wiliam Ferraciolli <wiliam334@hotmail.com>
2024-11-18 23:43:17 +0200

    material-theme: added overlay modal

--- material-themes/README.md
+++ material-themes/README.md
@@ -16,8 +16,49 @@ Some more details can be found here `https://levelup.gitconnected.com/defining-y
 Material `ng add @angular/material`
 
 ## Instructions
-Firstly import the
+Firstly create a new scss file next the styles.scss called themes.scss
+them within angular.json, on the styles section define in both places Eg
+```json
+{
+  "styles": [
+    "@angular/material/prebuilt-themes/indigo-pink.css",
+    "src/styles.scss",
+    "src/themes.scss"
+  ]
+}
+```
+within the theme file, import material variables Eg
+```scss
+@use '@angular/material' as mat;
 
+// add material core
+@include mat.core();
+
+// define the palettes, themes and set the default theme
+```
+
+Conditionally check that within the body tag, if the dark-theme is applied, then add the dark theme palette
+```scss
+@include mat.all-component-themes($light-theme);
+
+body.dark-theme {
+  @include mat.all-component-themes($dark-theme);
+}
+```
+
+Finally create a toggle to add or remove the dark-thme class to the body element causing the palettes to be switched
+app.component.ts
+```ts
+  public toggleTheme(): void {
+    if (document.body.classList.contains('dark-theme')) {
+      document.body.classList.remove('dark-theme');
+      document.body.classList.add('light-theme');
+    } else {
+      document.body.classList.remove('light-theme');
+      document.body.classList.add('dark-theme');
+    }
+  }
+```
 
 ### To apply the background color t the whole page
 Make sure to add the `mat-app-background` class to the app container and make it 100vh in order for the app to always 
@@ -35,4 +76,5 @@ Eg app.component.html
 }
 ```
 ### overlay items Eg Modal
-For the dialogs and overlay to also have themes applied, it is necessary to inject the OverlayContainer on the main component set the theme
+For the dialogs and overlay to also have themes applied, it is necessary to inject the OverlayContainer on 
+the main component and set the theme based ont the classes applied to the body 
--- material-themes/src/app/app.component.ts
+++ material-themes/src/app/app.component.ts
@@ -1,37 +1,53 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatDivider} from "@angular/material/divider";
 import {MatAnchor, MatButton, MatFabButton, MatIconButton, MatMiniFabButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
 import {MatToolbar, MatToolbarRow} from "@angular/material/toolbar";
 import {RouterOutlet} from "@angular/router";
-import {MatSlideToggle} from "@angular/material/slide-toggle";
 import {MatDialog} from "@angular/material/dialog";
-import {inject} from "@angular/core";
 import {DialogTemplateComponent} from "./dialog-template/dialog-template.component";
+import {OverlayContainer} from "@angular/cdk/overlay";
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from "@angular/material/card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatDivider, MatButton, MatAnchor, MatIcon, MatIconButton, MatFabButton, MatMiniFabButton, MatToolbar, MatToolbarRow, RouterOutlet, MatSlideToggle],
+  imports: [MatDivider, MatButton, MatAnchor, MatIcon, MatIconButton, MatFabButton, MatMiniFabButton, MatToolbar, MatToolbarRow, RouterOutlet, MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent, MatCardActions],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'material-themes';
 
-  public dialogService: MatDialog = inject(MatDialog);
+  private dialogService: MatDialog = inject(MatDialog);
+  private overlayService: OverlayContainer = inject(OverlayContainer);
 
   public toggleTheme(): void {
     if (document.body.classList.contains('dark-theme')) {
+      // apply light theme to the container app
       document.body.classList.remove('dark-theme');
       document.body.classList.add('light-theme');
+
+      // apply light theme to overlay
+      this.overlayService.getContainerElement().classList.add('light-theme');
     } else {
+      // apply dark theme to container app
       document.body.classList.remove('light-theme');
       document.body.classList.add('dark-theme');
+
+      // apply dark theme overlay
+      this.overlayService.getContainerElement().classList.add('dark-theme');
     }
   }
 
-  public openDialog(){
+  public openDialog() {
     this.dialogService.open(DialogTemplateComponent, {
       width: '250px'
     })
