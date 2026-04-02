d46328cb735a88d957ee81ac10244d96a0580eb1
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2026-04-02 13:22:16 +0100

    signal form arrays - Added dynamic arrays

--- signal-form-array/src/app/app.routes.ts
+++ signal-form-array/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {ContactForm} from './forms/contacts/contact-form/contact-form';
 import {Home} from './home/home';
 import {CrossFieldForm} from './forms/cross-fields/cross-field-form/cross-field-form';
 import {DynamicSampleForm} from './forms/dynamic/dynamic-sample-form/dynamic-sample-form';
+import {ArrayForm} from './forms/arrays/array-form/array-form';
 
 export const routes: Routes = [
   {
@@ -18,6 +19,9 @@ export const routes: Routes = [
     component: CrossFieldForm,
   },
   {
+    path: 'forms/arrays',
+    component: ArrayForm,
+  },  {
     path: 'forms/dynamic-form',
     component: DynamicSampleForm,
   },
