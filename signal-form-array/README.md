c0a5d1619b7bbe6ef518bf999e3da18e24b6428a
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2026-06-03 12:52:52 +0100

    signal form arrays - Added custome theme

--- signal-form-array/README.md
+++ signal-form-array/README.md
@@ -2,3 +2,11 @@
 
 ## Dependencies
 Angular Material
+
+
+### Custom Material theme
+Type the below and select the main attributes. Then it will create the cusom theme
+
+```bash
+    ng generate @angular/material:theme-color
+```
--- signal-form-array/src/app/app.ts
+++ signal-form-array/src/app/app.ts
@@ -1,15 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
+import {RouterOutlet, RouterLink} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+type ThemeMode = 'light' | 'dark';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
   protected readonly title = signal('Signal Forms');
+  protected readonly isDark = signal(false);
+
+  constructor() {
+    const theme: string | null = localStorage.getItem('theme');
+    if (theme) {
+      this.isDark.set(true);
+      this._setDarkMode();
+    }
+  }
+
+  protected toggleDarkMode(): void {
+    this._setDarkMode();
+    const isDarkTheme = document.body.classList.contains('dark-mode');
+
+    if (isDarkTheme) {
+      this.isDark.set(true);
+      localStorage.setItem('theme', 'dark');
+    } else {
+      this.isDark.set(false);
+      localStorage.removeItem('theme');
+    }
+  }
+
+  private _setDarkMode(): void {
+    document.body.classList.toggle('dark-mode');
+  }
 }
