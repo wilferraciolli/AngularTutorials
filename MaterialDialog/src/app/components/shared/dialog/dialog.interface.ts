f350cb189735301678a44035b1e32a7650447285
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2026-09-25 13:29:45 +0100

    deleted olf projects

--- MaterialDialog/src/app/components/shared/dialog/dialog.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface DialogInterface {
-  cancelButtonLabel: string;
-  confirmButtonLabel: string;
-  dialogHeader: string;
-  dialogContent: string;
-  callbackMethod: () => void;
-}
--- storybook-template/src/app/ui/user-details/components/user-details/user-details.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface UiUserDetails {
-  firstName: string;
-  lastName: string;
-  email: string;
-  avatar: {
-    url: string;
-    alt: string;
-  } | null;
-}
